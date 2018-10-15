ed7d35009cbfe249a1cc9769422d7adb4cb41f94
[filipe] <[luisfilipenascimento7@gmail.com]>
2018-10-15 20:26:41 -0300

    Alterações dois no arquivo functions.php

--- functions.php
+++ functions.php
@@ -3,3 +3,15 @@ function umaFuncao() {
 echo “Uma funcao que imprime”.PHP_EOL;
 }
 ?>
+<?php
+function outraFuncao($nome) {
+echo “Olá, {$nome}!”.PHP_EOL;
+
+function outraFuncao($nome) {
+echo “Olá, {$nome}! Eu sou uma funcao!”.PHP_EOL;
+}
+
+function novaFuncao($msg) {
+echo “Sou uma funcao que recebe: {$msg}”.PHP_EOL;
+}
+?>
